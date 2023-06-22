e524d607ea4df8374393df5371903c3f0f4dd5b7
rishverma1 <95046533+rishverma1@users.noreply.github.com>
2023-06-22 16:09:47 +0530

    ec2added

--- scripts/ec2.sh
+++ scripts/ec2.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-aws opsworks --region us-east-1 create-instance --stack-id 935450cc-61e0-4b03-a3e0-160ac817d2bb --layer-ids 5c8c272a-f2d5-42e3-8245-5bf3927cb65b --hostname myinstance1 --instance-type t2.micro --os "Amazon Linux"
+aws ec2 run-instances \
+    --image-id ami-022e1a32d3f742bd8 \
+    --count 1 \
+    --instance-type t2.micro \
+    --key-name githbactions-key \
+    --security-group-ids sg-0ac7c6ba0069b099c \
+    --subnet-id subnet-010062032777ab1f9 \
+    --block-device-mappings "[{\"DeviceName\":\"/dev/sdf\",\"Ebs\":{\"VolumeSize\":30,\"DeleteOnTermination\":false}}]" \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=demo-server}]' 'ResourceType=volume,Tags=[{Key=Name,Value=demo-server-disk}]'
